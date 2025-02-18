ea19b59c64f82c239449362b103655e72bdb2b87
Shradd7 <157452974+Shradd7@users.noreply.github.com>
2025-02-18 11:00:34 +0000

    variable done

---
+++ basics/variable.js
@@ -0,0 +1,12 @@
+const accountId = 144
+var accountName = "Shraddhan"
+let accountPass = "1234a"
+accountType = "Professional"
+//Use let for variable and const for constant 
+// accountId = 2 Not Allowed
+accountName = "Shradd"
+accountPass = "Abcd"
+accountType = "Public"
+
+console.log(accountId);
+console.table([accountId,accountName,accountPass,accountType])
